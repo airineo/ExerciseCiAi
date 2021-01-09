de1693539b9545e9bc64c27b36b00f4bc269c7bb
airineo <alberto.irineo@gmail.com>
2021-01-09 14:15:15 -0600

    avance de solución de métodos, mapeado y filtrado de headers

--- ExerciseCiAi/src/main/java/com/main/exercise/controller/ExerciseController.java
+++ ExerciseCiAi/src/main/java/com/main/exercise/controller/ExerciseController.java
@@ -1,15 +1,19 @@
 package com.main.exercise.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -23,25 +27,26 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/ci")
 public class ExerciseController {
-	
-	
+
+
 	/**
 	 * Método que mapea headers.
 	 * @param request HttpServletRequest objecto
 	 * @return 
 	 */
 	@GetMapping("/getHeaders")
-	public Map<String, List<String>> getHeaders(HttpServletRequest request) {
-		System.out.println("HEADER NAMES : ");
-		System.out.println(request.getHeaderNames());
-		System.out.println("HEADERs prueba : ");
-		System.out.println(request.getHeaders("prueba"));
+	public Map<String, List<String>> getHeaders(HttpServletRequest request) {	
 		Map<String, List<String>> map = new Hashtable<String, List<String>>();
-		List<String> list = new ArrayList<>();
-		list.add("hola");
-		list.add("alberto");
-		map.put("prueba", list);
-		
+
+		Stream<String> headerNames = (Stream<String>)
+				Collections.list(request.getHeaderNames())
+				.stream();
+		headerNames.forEach(element ->{
+			List<String> list = new ArrayList<>();
+			list.add(request.getHeader(element));
+			map.put(element, list);
+		});
+
 		return map;
 	}
 	/**
@@ -51,7 +56,20 @@ public class ExerciseController {
 	 * @return Map<String, List<String>> Devuelve un mapa de los elementos filtrados en base a headersNames
 	 */
 	@GetMapping("/filteredHeaders")
-	public Map<String, List<String>> filteredHeaders(HttpServletRequest request, List<String> headerNames) {
-		return null;
+	public Map<String, List<String>> filteredHeaders(HttpServletRequest request, @RequestParam("headerNames") List<String> headerNames) {
+		Map<String, List<String>> map = new Hashtable<String, List<String>>();
+		Stream<String> headerNames1 = (Stream<String>)
+				Collections.list(request.getHeaderNames())
+				.stream();
+		
+		headerNames.forEach(element ->{
+			headerNames1.filter(header -> header == element);
+			List<String> list = new ArrayList<>();
+			list.add(request.getHeader(element));
+			map.put(element, list);
+		});
+
+
+		return map;
 	}
 }
